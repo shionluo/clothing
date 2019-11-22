a911128d372ea7e495b7a18147bae2d7a266d801
Shion <shionchill@Shions-MacBook-Pro.local>
2019-11-22 11:16:53 +0700

    Initial Project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//-- Component --//
+import App from "./App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+//-- Style --//
+import "./index.css";
+
+//-- Service Worker --//
+import * as serviceWorker from "./serviceWorker";
+
+//-----------------------------------------------------------------------------//
+//-----------------------------------------------------------------------------//
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
 serviceWorker.unregister();
